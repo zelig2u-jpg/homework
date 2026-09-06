ac29046139fad3158fc0e2cdc964928de9a7d91a
Zelig <Zelig2u@gmail.com>
2026-09-06 02:39:49 -0400

    new file:   63/hw.html 	new file:   63/messageBox.js 	new file:   63/useMessageBox.js

---
+++ js/63/useMessageBox.js
@@ -0,0 +1,12 @@
+import showMessage from "./messageBox.js";
+
+showMessage('it works!');
+
+
+const msgInput = document.querySelector('#msg');
+
+document.querySelector('#showMessage').addEventListener('submit', (e) => {
+  e.preventDefault();
+  buttons: ['yes', 'no', 'maybe'],
+  showMessage(msgInput.value);
+});
